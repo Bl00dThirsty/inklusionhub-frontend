d6a33a9a4fe07bb976822ecfab8c3a0e39b11f60
yvana2005 <ivanaazambou@gmail.com>
2025-11-28 17:44:18 +0100

    creation de la sign-in et de la sign-up page

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,8 +1,10 @@
 "use client";
-
+import { useRouter, useParams } from 'next/navigation';
+import { useState, useEffect } from "react"
 import Image from "next/image";
 
 export default function Header() {
+  const router = useRouter();
   return (
     <header className="w-full bg-white shadow-sm py-4 px-8 flex items-center justify-between">
       {/* Logo */}
@@ -23,7 +25,9 @@ export default function Header() {
       {/* Actions */}
       <div className="flex gap-2">
         <button className="px-4 py-1 rounded-full text-sm text-white" style={{ backgroundColor: "#82EFCF" }}>S'inscrire</button>
-        <button className="px-4 py-1 bg-blue-600 text-white rounded-full text-sm">
+        <button 
+          onClick={() => router.push(`/sign-in/`)}
+          className="px-4 py-1 bg-blue-600 text-white rounded-full text-sm">
           Connexion
         </button>
       </div>
